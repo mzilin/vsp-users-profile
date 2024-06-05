90fdfe3469b9ad1a92bb50c3a645a7402d121115
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-06 00:51:23 +0100

    init user profile service

---
+++ src/main/java/com/mariuszilinskas/vsp/userprofileservice/VspUserProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mariuszilinskas.vsp.userprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspUserProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VspUserProfileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariuszilinskas/vsp/userprofileservice/VspUserProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariuszilinskas.vsp.userprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VspUserProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
