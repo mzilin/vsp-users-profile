0478d69baf1f8ce062fce17001c630f29541841e
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-12 19:20:16 +0100

    create UserProfileService interface

---
+++ src/main/java/com/mariuszilinskas/vsp/userprofileservice/service/UserProfileService.java
@@ -0,0 +1,21 @@
+package com.mariuszilinskas.vsp.userprofileservice.service;
+
+import com.mariuszilinskas.vsp.userprofileservice.dto.CreateUserProfileRequest;
+import com.mariuszilinskas.vsp.userprofileservice.model.UserProfile;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserProfileService {
+
+    UserProfile createUserProfile(CreateUserProfileRequest request);
+
+    List<UserProfile> getAllUserProfiles(UUID userId);
+
+    UserProfile getUserProfile(UUID userId, UUID profileId);
+
+    UserProfile updateUserProfile(UUID userId, UUID profileId, CreateUserProfileRequest request);
+
+    void deleteUserProfile(UUID userId, UUID profileId);
+
+}
