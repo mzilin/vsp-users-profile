ba41aaaa27644a0f3a6fe2e664f0028304ff5b05
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-12 21:17:37 +0100

    update UserProfileRepository

--- src/main/java/com/mariuszilinskas/vsp/userprofileservice/repository/UserProfileRepository.java
+++ src/main/java/com/mariuszilinskas/vsp/userprofileservice/repository/UserProfileRepository.java
@@ -4,6 +4,8 @@ import com.mariuszilinskas.vsp.userprofileservice.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -13,5 +15,15 @@ import java.util.UUID;
  */
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+
+    boolean existsByUserIdAndProfileName(UUID userId, String profileName);
+
+    boolean existsByUserIdAndProfileNameAndIdNot(UUID userId, String profileName, UUID id);
+
+    List<UserProfile> findAllByUserId(UUID userId);
+
+    Optional<UserProfile> findByIdAndUserId(UUID profileId, UUID userId);
+
+    void deleteAllByUserId(UUID userId);
 
 }
