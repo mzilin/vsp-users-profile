47a0fbdb46d1c36193f35f21668bb2e32f02589b
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-12 02:08:20 +0100

    create file upload exceptions and update handler

--- src/main/java/com/mariuszilinskas/vsp/userprofileservice/handler/AppExceptionHandler.java
+++ src/main/java/com/mariuszilinskas/vsp/userprofileservice/handler/AppExceptionHandler.java
@@ -2,7 +2,9 @@ package com.mariuszilinskas.vsp.userprofileservice.handler;
 
 import com.mariuszilinskas.vsp.userprofileservice.dto.ErrorResponse;
 import com.mariuszilinskas.vsp.userprofileservice.dto.FieldErrorResponse;
+import com.mariuszilinskas.vsp.userprofileservice.exception.EntityExistsException;
 import com.mariuszilinskas.vsp.userprofileservice.exception.FileUploadException;
+import com.mariuszilinskas.vsp.userprofileservice.exception.IncorrectFileException;
 import com.mariuszilinskas.vsp.userprofileservice.exception.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +44,11 @@ public class AppExceptionHandler {
 
     // --------------------- General ------------------------------
 
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEntityExistsException(EntityExistsException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return buildErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
@@ -54,10 +61,14 @@ public class AppExceptionHandler {
 
     // --------------------- Specific -----------------------------
 
-
     @ExceptionHandler(FileUploadException.class)
     public ResponseEntity<ErrorResponse> handleFileUploadException(FileUploadException ex) {
         return buildErrorResponse(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IncorrectFileException.class)
+    public ResponseEntity<ErrorResponse> handleIncorrectFileException(IncorrectFileException ex) {
+        return buildErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     // -----------------------------------------------------------
