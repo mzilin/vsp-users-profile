7a56c94a16cdb1c51faa069c589748da30a427cd
Marius Zilinskas <mariuzmail@gmail.com>
2024-06-21 13:57:16 +0100

    set up RabbitMQConsumer

---
+++ src/main/java/com/mariuszilinskas/vsp/userprofileservice/consumer/RabbitMQConsumer.java
@@ -0,0 +1,32 @@
+package com.mariuszilinskas.vsp.userprofileservice.consumer;
+
+import com.mariuszilinskas.vsp.userprofileservice.dto.CreateUserDefaultProfileRequest;
+import com.mariuszilinskas.vsp.userprofileservice.service.UserProfileService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQConsumer.class);
+    private final UserProfileService userProfileService;
+
+    @RabbitListener(queues = "${rabbitmq.queues.profile-setup}")
+    public void consumeCreateDefaultUserProfileMessage(CreateUserDefaultProfileRequest request) {
+        logger.info("Received request to create user profile: {}", request);
+        userProfileService.createDefaultUserProfile(request);
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queues.delete-user-data}")
+    public void consumeDeleteUserDataMessage(UUID userId) {
+        logger.info("Received request to delete user data for User [userId: {}]", userId);
+        userProfileService.deleteAllUserProfiles(userId);
+    }
+
+}
